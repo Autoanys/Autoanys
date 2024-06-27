abfa009e6dccbcdb91c88586e5d99c1ceb237759
unknown <developer.charleslo@gmail.com>
2024-06-27 12:33:10 +0800

    add : hover shadow dashboard items

--- frontend/src/components/Charts/ExecutionTableOne.tsx
+++ frontend/src/components/Charts/ExecutionTableOne.tsx
@@ -192,7 +192,7 @@ const ExecutionTableOne: React.FC = (tData) => {
   handleReset;
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
+    <div className="col-span-12  rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default hover:shadow-xl dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
       <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
         <div className="flex w-full flex-wrap gap-3 sm:gap-5">
           <div className="flex min-w-47.5">
--- frontend/src/components/Charts/ExecutionTableTwo.tsx
+++ frontend/src/components/Charts/ExecutionTableTwo.tsx
@@ -89,7 +89,7 @@ const ExecutionTableTwo: React.FC = (tData) => {
   handleReset;
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
+    <div className="col-span-12  rounded-sm border border-stroke bg-white p-7.5 shadow-default hover:shadow-xl dark:border-strokedark dark:bg-boxdark xl:col-span-4">
       <div className="mb-4 justify-between gap-4 sm:flex">
         <div>
           <h4 className="text-xl font-semibold text-black dark:text-white">
