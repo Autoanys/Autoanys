69c969ca2594b46f8722ebaad61e2bc8d4b3c648
unknown <developer.charleslo@gmail.com>
2024-07-17 16:03:47 +0800

    fix : theme for plugins configs

--- frontend/src/components/Extensions/ExtensionsSetting.tsx
+++ frontend/src/components/Extensions/ExtensionsSetting.tsx
@@ -8,6 +8,7 @@ const ExtensionsSetting = (pluginID) => {
   const [profiles, setProfiles] = useState([
     { id: 1, name: "Default Name", default: true, input: {} },
   ]);
+
   const [resultLoading, setResultLoading] = useState(true);
   const [showNotification, setShowNotification] = useState({
     show: false,
@@ -120,77 +121,91 @@ const ExtensionsSetting = (pluginID) => {
 
   return (
     <div className=" flex h-[calc(100vh-200px)] p-4">
-      <div className="flex h-full w-1/3  flex-col rounded-lg bg-white shadow-xl dark:bg-boxdark">
-        <div className="p-4">
-          <h2 className="mb-2 flex items-center text-xl font-bold">
-            <img src={plugins.imageUrl} className="mr-2 h-5 w-5" alt="plugin" />
-            {plugins.name}
-          </h2>
-          <p className="mb-4">{plugins.title}</p>
-          {/* <a target="_blank" href={plugins.doc}>
+      {resultLoading && (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <div className="border-gray-900 h-32 w-32 animate-spin rounded-full border-b-2 border-t-2"></div>
+        </div>
+      )}
+
+      {!resultLoading && (
+        <div className="flex h-full w-1/3  flex-col rounded-l-lg border border-slate-300 bg-white shadow-xl dark:bg-boxdark">
+          <div className="p-4">
+            <h2 className="mb-2 flex items-center text-xl font-bold">
+              <img
+                src={plugins.imageUrl}
+                className="mr-2 h-5 w-5"
+                alt="plugin"
+              />
+              {plugins.name}
+            </h2>
+            <p className="mb-4">{plugins.title}</p>
+            {/* <a target="_blank" href={plugins.doc}>
             ℹ️ Documentation
           </a> */}
-          <button
-            className="w-full rounded-lg bg-blue-500 p-4 px-4 py-2  text-white"
-            onClick={addProfile}
-          >
-            Add Profile
-          </button>
-        </div>
-        <ul className="flex-1 overflow-auto p-4 ">
-          {profiles.map((profile) => (
-            <li
-              key={profile.id}
-              className={`cursor-pointer rounded-lg p-2 ${selectedProfile.id === profile.id ? "bg-slate-100	 font-medium dark:bg-meta-4" : ""}`}
-              onClick={() => setSelectedProfile(profile)}
+            <button
+              className="w-full rounded-l-lg bg-blue-500 p-4 px-4 py-2  text-white"
+              onClick={addProfile}
             >
-              {profile.default && "⭐"} {profile.name}
-            </li>
-          ))}
-        </ul>
-        <div className="mt-auto flex justify-between">
-          <button
-            className="w-1/2 rounded-bl-lg bg-slate-500 px-4 py-2 text-white"
-            onClick={() => markDefault(selectedProfile.id)}
-          >
-            {selectedProfile && selectedProfile.default ? "⭐" : "★"} Default
-          </button>
-          <button
-            className="w-1/2 rounded-br-lg bg-rose-600 px-4 py-2 text-white"
-            onClick={() => deleteProfile(selectedProfile.id)}
-          >
-            🗑️ Delete
-          </button>
-        </div>
-      </div>
-      <div className="flex h-full w-2/3 flex-col rounded-lg bg-white p-4 shadow-xl dark:bg-boxdark">
-        <h2 className="mb-4 text-xl font-bold">Profile Details</h2>
-        {configInput.length > 0 &&
-          configInput.map((input) => (
-            <div className="mb-4">
-              <label
-                className="mb-1 block text-sm font-medium"
-                htmlFor={input.name}
+              Add Profile
+            </button>
+          </div>
+          <ul className="flex-1 overflow-auto p-4 ">
+            {profiles.map((profile) => (
+              <li
+                key={profile.id}
+                className={`cursor-pointer rounded-lg p-2 ${selectedProfile.id === profile.id ? "bg-slate-100	 font-medium dark:bg-meta-4" : ""}`}
+                onClick={() => setSelectedProfile(profile)}
               >
-                {input.label}
-              </label>
-              <input
-                type={input.type}
-                id={selectedProfile.id + input.name}
-                className="w-full rounded-lg border p-2"
-                value={input.value}
-              />
-            </div>
-          ))}
-        <div className="mt-auto flex justify-end ">
-          <button className=" rounded-lg bg-blue-500 px-4 py-2 text-white">
-            🧪 Test
-          </button>
-          <button className="ml-2 rounded-lg bg-green-600 px-4 py-2 text-white">
-            💾 Save
-          </button>
+                {profile.default && "⭐"} {profile.name}
+              </li>
+            ))}
+          </ul>
+          <div className="mt-auto flex justify-between">
+            <button
+              className="w-1/2 rounded-bl-lg bg-slate-500 px-4 py-2 text-white"
+              onClick={() => markDefault(selectedProfile.id)}
+            >
+              {selectedProfile && selectedProfile.default ? "⭐" : "★"} Default
+            </button>
+            <button
+              className="w-1/2  bg-rose-600 px-4 py-2 text-white"
+              onClick={() => deleteProfile(selectedProfile.id)}
+            >
+              🗑️ Delete
+            </button>
+          </div>
         </div>
-      </div>
+      )}
+      {!resultLoading && (
+        <div className="flex h-full w-2/3 flex-col rounded-r-lg border-b border-r border-t border-slate-300 bg-white p-4 shadow-xl dark:bg-boxdark">
+          <h2 className="mb-4 text-xl font-bold">Profile Details</h2>
+          {configInput.length > 0 &&
+            configInput.map((input) => (
+              <div className="mb-4">
+                <label
+                  className="mb-1 block text-sm font-medium"
+                  htmlFor={input.name}
+                >
+                  {input.label}
+                </label>
+                <input
+                  type={input.type}
+                  id={selectedProfile.id + input.name}
+                  className="w-full rounded-lg border p-2"
+                  value={input.value}
+                />
+              </div>
+            ))}
+          <div className="mt-auto flex justify-end ">
+            <button className=" rounded-lg bg-blue-500 px-4 py-2 text-white">
+              🧪 Test
+            </button>
+            <button className="ml-2 rounded-lg bg-green-600 px-4 py-2 text-white">
+              💾 Save
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
